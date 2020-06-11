eabfdefaed6fbe0eee4a836d70400cd29728091a
Ahmed Mahmoud <ahmedasiawy@gmail.com>
2020-06-11 10:36:36 +0200

    Heroku database config - adding django-heroku

--- DCmonitor/settings.py
+++ DCmonitor/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -137,3 +139,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# heroku database setup
+django_heroku.settings(locals())
